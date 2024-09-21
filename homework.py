658961bde248b4052698b94f6d1fa72ec8eb490f
Valerij <valerijpetrencko@gmail.com>
2024-09-21 21:38:59 +0300

    Финал

--- homework.py
+++ homework.py
@@ -1,12 +1,30 @@
-...
+import logging
+import os
+import sys
+import time
+from http import HTTPStatus
+
+import requests
+from dotenv import load_dotenv
+from telebot import TeleBot
+
+from exceptions import (
+    SendError, RequestError, ParseError
+)
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
-
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -20,47 +38,88 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверяет наличие данных в окружении."""
+    try:
+        assert (
+            all([PRACTICUM_TOKEN, TELEGRAM_CHAT_ID, TELEGRAM_TOKEN]) is True
+        )
+    except Exception as error:
+        logging.critical(
+            f'Ошибка при чтении токенов. {error}'
+            'Программа была остановлена'
+        )
+        sys.exit()
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщение от бота."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logging.debug(f'Бот отправил сообщение: {message}')
+    except Exception as error:
+        raise SendError from error
 
 
 def get_api_answer(timestamp):
-    ...
+    """Получает ответ от сервера."""
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params={'from_date': timestamp}
+        )
+        assert response.status_code == HTTPStatus.OK
+    except Exception as error:
+        raise RequestError(response.status_code, error) from error
+    else:
+        return response.json()
 
 
 def check_response(response):
-    ...
+    """Проверяет ответ от сервера."""
+    try:
+        assert type(response.get('homeworks')) == list
+    except Exception as error:
+        raise TypeError from error
+    else:
+        if response.get('homeworks') != []:
+            return True
+        logging.debug('Статус работы не изменился')
 
 
 def parse_status(homework):
-    ...
-
+    """Возвращает статус работы."""
+    try:
+        status = homework['status']
+        verdict = HOMEWORK_VERDICTS[status]
+        homework_name = homework['homework_name']
+    except Exception as error:
+        raise ParseError from error
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
-    ...
+    check_tokens()
 
     while True:
         try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            response = get_api_answer(timestamp)
+            if check_response(response):
+                message = parse_status(response['homeworks'][0])
+                send_message(bot, message)
+        except RequestError as error:
+            logging.error(f'Страница недоступна. {error}')
+        except SendError as error:
+            logging.error(f'При отправке ответа возникла ошибка. {error}')
+        except TypeError as error:
+            logging.error(f'Некорректный ответ от сервера. {error}')
+        except ParseError as error:
+            logging.error(error)
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
