839528883387b025fd342fd123e41f51f9ac5027
Valerij Petrencko <valerijpetrencko@gmail.com>
2024-10-03 00:32:10 +0300

    Отправка на 3 ревью

--- exceptions.py
+++ exceptions.py
@@ -1,22 +1,9 @@
 class RequestError(Exception):
     """Некорректный запрос к серверу."""
 
-    def __init__(self, status, request_data):
-        """Инициализация исключения."""
-        self.status = status
-        self.request_data = request_data
-        super().__init__(
-            f'Неверный статус ответа {status}'
-            f'Данные запроса: {request_data}'
-        )
-
 
 class SendError(Exception):
     """Ошибка при отправке сообщения."""
-
-    def __init__(self, message):
-        """Инициализация исключения."""
-        super().__init__(message)
 
 
 class ParseError(Exception):
--- homework.py
+++ homework.py
@@ -26,20 +26,25 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+TOKENS = {
+    'Токен Практикума': PRACTICUM_TOKEN,
+    'ID чата': TELEGRAM_CHAT_ID,
+    'Токен Телеграмма': TELEGRAM_TOKEN,
+}
 
-def check_tokens() -> bool:
+
+def check_tokens(tokens: dict) -> bool:
     """Проверяет наличие данных в окружении."""
-    tokens = {
+    TOKENS = {
         'Токен Практикума': PRACTICUM_TOKEN,
         'ID чата': TELEGRAM_CHAT_ID,
         'Токен Телеграмма': TELEGRAM_TOKEN,
     }
-    values = [i for i in tokens.values()]
-    if all(values) is True:
-        return True
-    tokens_failure = [name for name, token in tokens.items() if token is None]
-    logging.critical(f'Отсутствуют {tokens_failure}')
-    return False
+    tokens_failure = [name for name, token in TOKENS.items() if token is None]
+    if tokens_failure:
+        logging.critical(f'Отсутствуют {tokens_failure}')
+        return False
+    return True
 
 
 def send_message(bot: TeleBot, message: str) -> None:
@@ -58,7 +63,7 @@ def send_message(bot: TeleBot, message: str) -> None:
 def get_api_answer(timestamp: int) -> dict:
     """Получает ответ от сервера."""
     request_kwargs = {
-        'endpoint': ENDPOINT,
+        'url': ENDPOINT,
         'headers': HEADERS,
         'params': {'from_date': timestamp},
     }
@@ -66,18 +71,13 @@ def get_api_answer(timestamp: int) -> dict:
         logging.debug('Начато обращение к серверу')
         response = requests.get(**request_kwargs)
         logging.debug('Ответ получен')
-        if response.status_code != HTTPStatus.OK:
-            raise RequestError(response.status_code, request_kwargs)
     except requests.RequestException as error:
-        logging.error(f'Ошибка при запросе. {error}')
-    except RequestError as error:
-        logging.error(
-            f'Неверный статус ответа {response.status_code}'
-            f'Данные запроса: {request_kwargs}'
+        raise RequestError(f'Ошибка во время выполнения запроса: {error}')
+    if response.status_code != HTTPStatus.OK:
+        raise RequestError(
+            f'Ошибочный статус{response.status_code, request_kwargs}'
         )
-        raise RequestError from error
-    else:
-        return response.json()
+    return response.json()
 
 
 def check_response(response: dict) -> bool:
@@ -87,12 +87,14 @@ def check_response(response: dict) -> bool:
             f'Получен неправильный ответ{response}'
             'Ответ не является словарем.'
         )
-        raise TypeError
+        raise TypeError('Ответ сервера получен не в виде словаря.')
+    if 'homeworks' not in response:
+        raise TypeError('В ответе отсутствует ключ "homeworks"')
     if not isinstance(response.get('homeworks'), list):
         logging.error('Значение "homeworks" не является списком.')
-        raise TypeError
-    if response.get('homeworks') == []:
-        logging.debug('Статус работы не изменился')
+        raise TypeError('Значение "homeworks" не является списком.')
+    if not response.get('homeworks'):
+        logging.debug('Статус работы не изменился.')
         return False
     return True
 
@@ -101,35 +103,40 @@ def parse_status(homework: dict) -> str:
     """Возвращает статус работы."""
     status = homework.get('status')
     homework_name = homework.get('homework_name')
-    if not homework_name:
-        raise ParseError
-    if status in HOMEWORK_VERDICTS:
-        verdict = HOMEWORK_VERDICTS[status]
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    logging.error(f'Неизвестный статус работы {status}')
-    raise ParseError
+    if not (status and homework_name):
+        raise ParseError(
+            f'Отсутствуют нужные ключи {status=}, {homework_name=}'
+        )
+    if status not in HOMEWORK_VERDICTS:
+        logging.error(f'Неизвестный статус работы {status}.')
+        raise ParseError('Неизвестный статус работы.')
+    return (
+        f'Изменился статус проверки работы "{homework_name}".'
+        f'{HOMEWORK_VERDICTS[status]}'
+    )
 
 
 def main() -> None:
     """Основная логика работы бота."""
-    if not check_tokens():
+    if not check_tokens(TOKENS):
         logging.critical('Программа завершает работу')
         sys.exit()
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
+    message = ''
     while True:
         try:
             response = get_api_answer(timestamp)
             if check_response(response):
-                message = parse_status(response['homeworks'][0])
-                send_message(bot, message)
+                if message != (message := parse_status(response['homeworks'][0])):
+                    send_message(bot, message)
         except Exception as error:
-            send_message(bot, f'Во время работы возникли ошибки. {error}')
+            logging.error(f'Произошла ошибка: {error}')
+            if message != (message := f'При работе возникли ошибки: {error}'):
+                send_message(bot, message)
         finally:
-            current_date = response.get('current_date')
             logging.debug(f'Старая дата запроса {timestamp}')
-            timestamp = current_date if current_date else timestamp
+            timestamp = response.get('current_date') or timestamp
             logging.debug(f'Новая дата запроса {timestamp}')
             logging.debug(f'Следующий запрос будет через {RETRY_PERIOD}')
             time.sleep(RETRY_PERIOD)
